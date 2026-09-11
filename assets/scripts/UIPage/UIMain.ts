2e099523658f7a64ebf521fd7bcde6beacd525ef
ANSWANG <2444683349@qq.com>
2026-09-11 11:52:22 +0800

    增加攻击获得

--- assets/scripts/UIPage/UIMain.ts
+++ assets/scripts/UIPage/UIMain.ts
@@ -167,6 +167,8 @@ export class UIMain extends UIBase {
 
     /**点击排行榜 */
     clickRankBtn() {
+        uiMgr.openPage(UIPath.UIReward);
+        return;
         //有昵称和授权或者h5平台才直接打开排行榜
         if ((gm.API.isAuthorize && userMgr.nickName) || gm.platType == PlatType.h5) {
             uiMgr.openPage(UIPath.UIRank);

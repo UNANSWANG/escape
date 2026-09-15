6a57b78c8e501f317e386036d322837adc6d81f2
ANSWANG <2444683349@qq.com>
2026-09-15 17:21:57 +0800

    增加属性赋值

--- assets/scripts/UIPage/UISign.ts
+++ assets/scripts/UIPage/UISign.ts
@@ -10,6 +10,15 @@ export class UISign extends UIBase {
     @property(Node)
     closeBtn: Node;
 
+    @property(Node)
+    getBtn: Node;
+
+    @property(Node)
+    adBtn: Node;
+
+    @property([Node])
+    itemList: Node[] = [];
+
     protected onLoad(): void {
         this.bindBtn();
     }
@@ -26,11 +35,22 @@ export class UISign extends UIBase {
 
     bindBtn() {
         this.closeBtn.addComponent(zoomButton).onClick = this.clickCloseBtn.bind(this);
+        this.getBtn.addComponent(zoomButton).onClick = this.clickGetBtn.bind(this);
+        this.adBtn.addComponent(zoomButton).onClick = this.clickAdBtn.bind(this);
     }
 
     ///
     ///点击事件
     ///
+    /**点击获取 */
+    clickGetBtn() {
+        console.log("点击获取");
+    }
+
+    /**点击广告 */
+    clickAdBtn() {
+        console.log("点击广告");
+    }
 
     /**点击关闭 */
     clickCloseBtn() {
